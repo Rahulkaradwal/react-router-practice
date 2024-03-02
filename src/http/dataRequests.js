99818fd7dec1188b4fe5ed7c2acc0df19ff7dc7f
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-03-02 05:48:30 -0500

    fix: loader getdata function

--- src/http/dataRequests.js
+++ src/http/dataRequests.js
@@ -1,6 +1,9 @@
 // Define a loader function to fetch some data
 export async function getData() {
-  const response = await fetch('https://localhost:8080/');
+  const response = await fetch('http://localhost:8080/events');
+  if (!response.ok) {
+    throw new Error('Could not load the Data');
+  }
   const userData = await response.json();
-  return userData;
+  return userData.events;
 }
